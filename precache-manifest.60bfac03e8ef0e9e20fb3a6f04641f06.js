d3d4a1615d3048443867e41d5d249576da80372c
mit510 <honestmit510@gmail.com>
2026-01-20 12:50:38 -0500

    Updates

---
+++ precache-manifest.60bfac03e8ef0e9e20fb3a6f04641f06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e80859c24e3b9eee600735dacda616a",
+    "url": "/portfolio_website/index.html"
+  },
+  {
+    "revision": "83d8464f8537a75800b8",
+    "url": "/portfolio_website/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "4b30fff21623cf5830f4",
+    "url": "/portfolio_website/static/css/main.8adf621f.chunk.css"
+  },
+  {
+    "revision": "83d8464f8537a75800b8",
+    "url": "/portfolio_website/static/js/2.a5417010.chunk.js"
+  },
+  {
+    "revision": "4b30fff21623cf5830f4",
+    "url": "/portfolio_website/static/js/main.1cc2813c.chunk.js"
+  },
+  {
+    "revision": "b53f768ed28665ffd266",
+    "url": "/portfolio_website/static/js/runtime-main.948ed5b9.js"
+  },
+  {
+    "revision": "02aee65440af5a48afc5173f93ce9b27",
+    "url": "/portfolio_website/static/media/Mitkumar_Patel.02aee654.pdf"
+  },
+  {
+    "revision": "f0d083edcf21f17bc306d3676f689771",
+    "url": "/portfolio_website/static/media/book.f0d083ed.jpg"
+  },
+  {
+    "revision": "c262e7fd697d04a3c623d8840d7da6a6",
+    "url": "/portfolio_website/static/media/cricket.c262e7fd.jpg"
+  },
+  {
+    "revision": "525fd7896c14aafebc154343346e9966",
+    "url": "/portfolio_website/static/media/foad_logo.525fd789.jpg"
+  },
+  {
+    "revision": "18d0ef12ab13f84ffecdb92299a6bca0",
+    "url": "/portfolio_website/static/media/programmer1.18d0ef12.png"
+  },
+  {
+    "revision": "579f480ce9a08b3d9114913b55678f58",
+    "url": "/portfolio_website/static/media/weather.579f480c.jpg"
+  }
+]);
